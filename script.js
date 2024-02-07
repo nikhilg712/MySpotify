d1098d01cdd4d87de4a395d576688443eaadf51b
Nikhil Gupta <nikhilg7404@gmail.com>
2024-02-07 22:32:02 +0530

    further changes to add some more functionalities in the app

--- script.js
+++ script.js
@@ -7,20 +7,31 @@ async function getSongs(){
     div.innerHTML = response;
     let as = div.getElementsByTagName("a");
     let song = [];
-    // for (let index = 0; index < as.length; index++) {
-    //     const element = as[index];
-    //     if(element.href.endsWith(".mp3"));
-    //     {
-    //         song.push(element.href);
-    //     }
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3"))
+        {
+            song.push(element.href.split("/songs/")[1]);
+        }
         
-    // }
-    console.log(as);
+    }
+    return song;
 }
 async function main(){
-    let song = await getSongs();
-    console.log(song);
-    // var audio = new Audio(song[3]);
-    // audio.play();
+    let songs = await getSongs();
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML+ `  <li>
+        <img class="music invert" src="music.svg" alt="music">
+        <div class="info">
+            <div>${song.replaceAll("%20"," ")}</div>
+            <div>Arijit Singh</div>
+        </div>
+        <div class="ply">Play Now</div>
+        <img src="plymusic.svg" alt="play now" class="playnow invert">
+    </li>`;
+    }
+  
+    console.log(songUL);
 };
 main();
